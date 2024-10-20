b464512c26d5313f69faebde261f374cd644710a
Tyrost <ccorzo306@gmail.com>
2024-10-20 03:57:00 -0700

    Updates

--- source/frontend/prognostication.php
+++ source/frontend/prognostication.php
@@ -1,3 +1,21 @@
+<?php
+
+require_once '../backend/PHP/commander.php';
+$data = [];
+$latitude = isset($_GET["lat"]) ? $_GET["lat"]:null;
+$longitude = isset($_GET["long"]) ? $_GET["long"]:null;
+$date = isset($_GET["date"]) ? $_GET["date"]:null;
+
+if ($latitude && $longitude && $date) {
+
+    $data = getPrognostication($latitude, $longitude, $date);
+
+    $data = $data ? json_decode($data, true) : [];
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,24 +24,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action= "" method="post" id="form-id">
+    <form action= "" method="get" id="form-id">
         <label for="lat">Latitude:</label><br>
         <input type="number" id="lat" name="lat"><br>
-        <label for="long">Longtitude:</label><br>
+        <label for="long">Longitude:</label><br>
         <input type="number" id="long" name="long">
 
-        <label for="start">Start Date</label><br>
-        <input type="date" id="start" name="start"><br>
-        <label for="lat">End Date</label><br>
-        <input type="date" id="end" name="end">
+        <label for="start">Date</label><br>
+        <input type="date" name="date"><br>
 
-        <label for="services">Choose:</label>
-        <select id="services" name="services">
-            <option value="" disabled selected>Span</option>
-            <option value="day">Day</option>
-            <option value="month">Month</option>
-            <option value="year">Year</option>
-        </select>
         <input type="submit" value="Submit">
       </form>
 </body>
--- source/index.php
+++ source/index.php
@@ -19,7 +19,7 @@
                 <button class="home-button" onclick="location.href='./index.php'">Home</button>
                 <button class="countries-button" onclick="location.href='./frontend/countries.php'">Countries</button>
                 <button class="rankings-button" onclick="location.href='./frontend/rankings.html'">Rankings</button>
-                <button class="Prognostication-button" onclick="location.href='./frontend/prognostication.html'">Prognostication</button>
+                <button class="Prognostication-button" onclick="location.href='./frontend/prognostication.php'">Prognostication</button>
                 <button class="newsletter-button" onclick="location.href='./frontend/newsletter.html'">Newsletter</button>
             </div>
     </div>
@@ -32,11 +32,65 @@
 
     <img class="main-image" src="./frontend/components/images/wildfire-image.jpg">
     </div>
+    <div style="width: 10px; height:200vh; color:white; position:absolute;"></div>
 
+    <div class="lower-image-container" id="lowerImageContainer">
+        <div class="panel-content">
+            <h2 style='font-size: 25px; font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;'>Climate Impact</h2>
+            <p style='line-height: 1.6; font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;top:40vh;'>
+                Learn how our environment is changing. Stay informed about the impact of global warming
+                on ecosystems and communities around the world.
+            </p>
+        </div>
+    </div>
 
-    <div class="lower-background-container"></div>
+    <div class="info-container">
+        <h2 class="info-title" style='color: white; text-align:center; top:5vh; font-size:50px; font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;'>Climate Change</h2>
+        <h2 class="info-title" style='color: white; text-align:center; font-size:18px; font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;'>The recurrent issue of the century</h2>
+        <div class="info-description-container">
+        <p style='line-height: 1.6; font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;; position: absolute; color:white; text-align: center; top:40vh; left: 30%;'>
+                Wildfires affect:<br>
+                    - Federal and State Budgets.<br>
+                    - Public Health.<br>
+                    - Natural environment.<br>
+                    <br>
+                    Increased emissions were linked to a rise in fire-favorable weather,<br>
+                    such as the hot, dry conditions seen during heat waves and droughts,<br>
+                    as well as increased rates of forest growth,<br>
+                    creating more vegetation fuels.<br>
+                    Both trends are aided by rapid warming in the high northern latitudes,<br>
+                    where it is happening twice as fast as the global average.<br>
+                    Boreal forests in Eurasia and North America,<br>
+                    saw emissions from fires nearly triple between 2001 and 2023.<br>
 
+            </p>
+        </div>
 
+    </div>
+
+    <div class="side-shape" id="shape-one"></div>
+    <div class="side-shape" id="shape-two"></div>
+
+<script>
+
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+  const panelSection = document.querySelector('.lower-image-container');
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition > 100 && scrollPosition > lastScrollTop) {
+    panelSection.classList.add('active');
+  }
+ 
+  else if (scrollPosition < 1000) {
+    panelSection.classList.remove('active');
+  }
+
+  lastScrollTop = scrollPosition;
+});
+
+</script>
 
 </body>
 </html>
